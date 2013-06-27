049351f9cfcc9a1ccd3c0893a24ea3f8a80de16b
Andrew Drummond <asdrummo@gmail.com>
2013-06-27 18:03:48 -0500

    paginate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -76,16 +76,18 @@ class ApplicationController < ActionController::Base
     
     def check_active(customer)
       @active = false
+      if customer.credits
       customer.credits.each do |credit|
         if credit.credits_available >= 1
           @active = true
         end
       end
+      end
     end
 
     unless Rails.application.config.consider_all_requests_local
-     # rescue_from Exception, with: lambda { |exception| render_error 500, exception }
-     # rescue_from ActionController::RoutingError, ActionController::UnknownController, ::AbstractController::ActionNotFound, ActiveRecord::RecordNotFound, with: lambda { |exception| render_error 404, exception }
+      rescue_from Exception, with: lambda { |exception| render_error 500, exception }
+      rescue_from ActionController::RoutingError, ActionController::UnknownController, ::AbstractController::ActionNotFound, ActiveRecord::RecordNotFound, with: lambda { |exception| render_error 404, exception }
     end
     
     def day_to_int(day)
